1539784fc4e3296b7490e5c8221573fd53c3b37a
GodJungGun <jg0428.lim@gmail.com>
2017-02-20 15:46:40 +0900

    bookmark & like model 삭제

--- app/models/bookmark.rb
+++
@@ -1,8 +0,0 @@
-class Bookmark < ApplicationRecord
-  #Reader(User) - Bookmark -Book relationship
-  belongs_to :reader, class_name: "User"
-  belongs_to :book
-
-  #Quote - Bookmark 1:N relationship
-  belongs_to :quote
-end
--- db/migrate/20170215100139_create_likes.rb
+++
@@ -1,9 +0,0 @@
-class CreateLikes < ActiveRecord::Migration[5.0]
-  def change
-    create_table :likes do |t|
-      t.belongs_to :user
-      t.belongs_to :opinion
-      t.timestamps
-    end
-  end
-end
