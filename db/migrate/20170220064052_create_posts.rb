bf5d193e4a635b3dd0ac3ae1a0e0f550cbba1d9c
GodJungGun <jg0428.lim@gmail.com>
2017-02-20 15:54:55 +0900

    migration file post/reply

--- db/migrate/20170220064052_create_posts.rb
+++ db/migrate/20170220064052_create_posts.rb
@@ -2,6 +2,11 @@ class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
 
+      t.string :content
+      t.integer :page
+      t.reference :user
+      t.reference :book
+
       t.timestamps
     end
   end
--- db/migrate/20170220064155_create_replies.rb
+++ db/migrate/20170220064155_create_replies.rb
@@ -2,6 +2,10 @@ class CreateReplies < ActiveRecord::Migration[5.0]
   def change
     create_table :replies do |t|
 
+      t.string :content
+      t.reference :post
+      t.reference :user
+
       t.timestamps
     end
   end
