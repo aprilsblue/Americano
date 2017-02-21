5c3ce1e953281d2b4fb71efc7510334820f90977
GodJungGun <jg0428.lim@gmail.com>
2017-02-21 17:22:18 +0900

    book/show:page구분

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220064155) do
+ActiveRecord::Schema.define(version: 20170221014426) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -35,6 +35,13 @@ ActiveRecord::Schema.define(version: 20170220064155) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "posts_tags", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "tag_id"
+    t.index ["post_id"], name: "index_posts_tags_on_post_id"
+    t.index ["tag_id"], name: "index_posts_tags_on_tag_id"
+  end
+
   create_table "replies", force: :cascade do |t|
     t.string   "content"
     t.integer  "post_id"
@@ -43,6 +50,12 @@ ActiveRecord::Schema.define(version: 20170220064155) do
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_replies_on_post_id"
     t.index ["user_id"], name: "index_replies_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
