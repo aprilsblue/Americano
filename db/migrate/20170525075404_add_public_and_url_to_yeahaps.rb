0ce5ed163e2939d4db841b0ed5cefc3b9f38a820
GodJungGun <jg0428.lim@gmail.com>
2017-05-28 06:07:03 +0900

    is_public default true -> 0

--- db/migrate/20170525075404_add_public_and_url_to_yeahaps.rb
+++ db/migrate/20170525075404_add_public_and_url_to_yeahaps.rb
@@ -1,6 +1,6 @@
 class AddPublicAndUrlToYeahaps < ActiveRecord::Migration[5.0]
   def change
-    add_column :yeahaps, :is_public, :boolean, default: "true"
+    add_column :yeahaps, :is_public, :boolean, default: 0
     add_column :yeahaps, :favicon_url, :string, default: "default"
   end
 end
