bd228adad4c8dab70b37a9d899d348e3135e768d
GodJungGun <jg0428.lim@gmail.com>
2017-05-27 14:44:57 +0900

    yeahapbox tool color:orange

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170525170449) do
+ActiveRecord::Schema.define(version: 20170525174659) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
