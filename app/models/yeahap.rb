c1ff3f30501b1d44db11e4b2ca36a4d7df0fcd78
uk6342 <uk6342@gmail.com>
2017-07-23 02:21:31 +0900

    slack webhook

--- app/models/yeahap.rb
+++ app/models/yeahap.rb
@@ -2,4 +2,26 @@ class Yeahap < ApplicationRecord
   belongs_to :user
   belongs_to :page
   belongs_to :yeahapbox
+
+  after_create :webhook
+
+  def webhook
+    if Yeahap.all.count%10 == 0
+      @yeahap = Yeahap.last
+      text="#{Yeahap.count}번째 예압이 만들어졌습니다. by #{@yeahap.user.email}"
+      notify_slack(text)
+    end
+  end
+
+  def notify_slack(text)
+    webhook_url = ENV["SLACK_WEBHOOK_URL"]
+    payload = {
+      :channel  => "#notice",
+      :username => "Yeahap-bot",
+      :text     => text,
+    }.to_json
+    cmd = "curl -X POST --data-urlencode 'payload=#{payload}' #{webhook_url}"
+    system(cmd)
+  end
+
 end
