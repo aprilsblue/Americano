f7b4769d5a11bb6d3b53701c1e0d3d8f7c596c1d
GodJungGun <jg0428.lim@gmail.com>
2017-08-09 17:44:56 +0900

    modify error

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
 
-  # :confirmable, :lockable, :timeoutable and :omniauthable :validatable, :confirmable,
+  # :confirmable, :lockable, :timeoutable and :omniauthable
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable,
          :omniauthable, :omniauth_providers => [:facebook]
 
   # Yeahap & pages
